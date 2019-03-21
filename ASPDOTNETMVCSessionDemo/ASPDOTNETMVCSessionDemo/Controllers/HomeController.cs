fef9dd50e8513671854713f591515d1600466fa0
Matthew Shaw <mshaw@sparta.com>
2019-03-21 20:25:00 +0000

    user login working

---
+++ ASPDOTNETMVCSessionDemo/ASPDOTNETMVCSessionDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPDOTNETMVCSessionDemo.Models;
+
+namespace ASPDOTNETMVCSessionDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //User us = new User();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
