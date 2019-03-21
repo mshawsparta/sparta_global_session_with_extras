56127f9ef0567e2fff2f52f3cdde499254302f22
Matthew Shaw <mshaw@sparta.com>
2019-03-21 20:58:16 +0000

    model added

--- ASPDOTNETMVCSessionDemo/ASPDOTNETMVCSessionDemo/Global.asax.cs
+++ ASPDOTNETMVCSessionDemo/ASPDOTNETMVCSessionDemo/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace ASPDOTNETMVCSessionDemo
 {
@@ -16,6 +18,7 @@ namespace ASPDOTNETMVCSessionDemo
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
